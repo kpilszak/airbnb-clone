1dd1bcf0037ac85c504839f2a3b7eacf153c98c2
kpilszak <klaudia.pilszak@gmail.com>
2023-02-17 06:22:24 -0500

    Fetch properties data

--- airbnb-clone-frontend/pages/index.js
+++ airbnb-clone-frontend/pages/index.js
@@ -10,7 +10,7 @@ const Home = ({ properties }) => {
 }
 
 export const getServerSideProps = async () => {
-  const query = '*[_type=="property"]';
+  const query = '*[ _type == "property"]';
   const properties = await sanityClient.fetch(query);
 
   if (!properties.length) {
